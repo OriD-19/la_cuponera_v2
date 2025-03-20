e1faf58fcc475332eb43f0481d28aac11f8e010c
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-03-20 00:18:20 -0600

    Added jwt middleware for correct jwt parsing

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { Hono } from 'hono';
 import login from './login';
 import offers from './offers';
 import register from './register';
+import { Variables } from '../schemas/jwtVariables';
 
 // for administrator only
 const adminApp = new Hono().basePath("/api/admin/v1");
 
-const app = new Hono().basePath("/api/v1");
+const app = new Hono<{ Variables: Variables }>().basePath("/api/v1");
 
 app.route("/login", login);
 app.route("/offers", offers);
